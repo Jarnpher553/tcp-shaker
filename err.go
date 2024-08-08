60b47575086ba69b2f52df064f31b90d7b3cc546
jarnpher_rice <jarnpher_553@foxmail.com>
2024-08-08 20:51:15 +0800

    fix: for windows

--- err.go
+++ err.go
@@ -1,10 +1,6 @@
 package tcp
 
-import (
-	"errors"
-
-	"golang.org/x/sys/unix"
-)
+import "errors"
 
 // ErrTimeout indicates I/O timeout
 var ErrTimeout = &timeoutError{}
@@ -15,16 +11,11 @@ func (e *timeoutError) Error() string   { return "I/O timeout" }
 func (e *timeoutError) Timeout() bool   { return true }
 func (e *timeoutError) Temporary() bool { return true }
 
+// ErrCheckerAlreadyStarted indicates there is another instance of CheckingLoop running.
+var ErrCheckerAlreadyStarted = errors.New("Checker was already started")
+
 // ErrConnect is an error occurs while connecting to the host
 // To get the detail of underlying error, lookup ErrorCode() in 'man 2 connect'
 type ErrConnect struct {
 	error
 }
-
-// newErrConnect returns a ErrConnect with given error code
-func newErrConnect(errCode int) *ErrConnect {
-	return &ErrConnect{unix.Errno(errCode)}
-}
-
-// ErrCheckerAlreadyStarted indicates there is another instance of CheckingLoop running.
-var ErrCheckerAlreadyStarted = errors.New("Checker was already started")
